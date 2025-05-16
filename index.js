4ef649d7ce7382cba13e14714cb4fddca7961257
ManiChandanaP <puvvadamanichandana@gmail.com>
2025-05-15 20:34:48 -0700

    Added basic index files

---
+++ index.js
@@ -0,0 +1,8 @@
+fetch("https://api.thedogapi.com/v1/breeds")
+  .then(response => response.json())
+  .then(data => {
+    console.log(data);
+  })
+  .catch(error => {
+    console.error("Error fetching dog data:", error);
+  });
